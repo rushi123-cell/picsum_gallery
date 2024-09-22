8e499804351ad82d38c0718adf88c13525170b81
DESKTOP-PT47CB4\raman <ramaniruchi7@gmail.com>
2024-09-22 10:22:08 +0530

    initial commit

--- lib/View/widget/image_card.dart
+++ lib/View/widget/image_card.dart
@@ -8,78 +8,15 @@ class ImageCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ImageDetailScreen(image: image),
-          ),
-        );
-      },
-      child: Hero(
-        tag: image.id!,
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          elevation: 6,
-          child: Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.network(
-                  image.downloadUrl!,
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: double.infinity,
-                ),
-              ),
-              Positioned(
-                bottom: 10,
-                left: 10,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  decoration: BoxDecoration(
-                    color: Colors.black54,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        image.author ?? 'Unknown',
-                        style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        '${image.width}x${image.height}',
-                        style: const TextStyle(color: Colors.white70),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
       ),
-    );
-  }
-}
-
-
-class ImageDetailScreen extends StatelessWidget {
-  final ImageModel image;
-
-  ImageDetailScreen({required this.image});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
+      elevation: 6,
+      child: Stack(
         children: [
-          Hero(
-            tag: image.id!,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
             child: Image.network(
               image.downloadUrl!,
               fit: BoxFit.cover,
@@ -88,36 +25,24 @@ class ImageDetailScreen extends StatelessWidget {
             ),
           ),
           Positioned(
-            top: 40,
-            left: 16,
-            child: IconButton(
-              icon: const Icon(Icons.arrow_back, color: Colors.white),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ),
-          Positioned(
-            bottom: 50,
-            left: 20,
-            right: 20,
+            bottom: 10,
+            left: 10,
             child: Container(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
               decoration: BoxDecoration(
                 color: Colors.black54,
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(12),
               ),
               child: Column(
-                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     image.author ?? 'Unknown',
-                    style: const TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                    style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
                   ),
-                  const SizedBox(height: 8),
                   Text(
-                    'Dimensions: ${image.width}x${image.height}',
-                    style: const TextStyle(color: Colors.white70, fontSize: 16),
+                    '${image.width}x${image.height}',
+                    style: const TextStyle(color: Colors.white70),
                   ),
                 ],
               ),
@@ -128,5 +53,4 @@ class ImageDetailScreen extends StatelessWidget {
     );
   }
 }
-
 
